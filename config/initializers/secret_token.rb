c10e6365fa4715f6499256fe332bbba3f4c0de46
Florian Duetsch <mail@florian-duetsch.de>
2011-06-23 15:10:09 +0200

    Rails-scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oreidig::Application.config.secret_token = '2e970951990d585ccec9b0c8a6fbf66327ed1f3aaac2899dfa20d08e58189095d8e198e5314a6c3828e96ad7a0e577c3bfd084cd1f22fdc070b84364071343a1'
