6f89f99fb97c8b407de3354e0fa6307533a151d5
Florian Duetsch <mail@florian-duetsch.de>
2011-09-13 18:32:31 +0200

    Removed warning

--- config/initializers/sprockets.rb
+++ config/initializers/sprockets.rb
@@ -1,11 +1,13 @@
 # TODO Flo: Blog this solution
-Oreidig::Application.assets.context_class.instance_eval do
+clz = Oreidig::Application.assets.context_class
+clz.instance_eval do
   include Rails.application.routes.url_helpers
   cattr_reader :default_url_options
-  # TODO Flo: warning: class variable access from toplevel
-  @@default_url_options = {
-    host: Settings.url_options.host,
-    port: Settings.url_options.port,
-    protocol: Settings.url_options.protocol
-  }
 end
+
+# prevent "warning: class variable access from toplevel"
+clz.class_variable_set '@@default_url_options', {
+                         host: Settings.url_options.host,
+                         port: Settings.url_options.port,
+                         protocol: Settings.url_options.protocol
+                       }
